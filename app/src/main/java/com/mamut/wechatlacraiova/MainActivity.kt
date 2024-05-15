74db833a0684e4ea0b978db5b4494bf6a7e8eb94
strumf007 <pozsonyistefan@gmail.com>
2024-05-15 19:37:26 +0300

    bumbam

--- app/src/main/java/com/mamut/wechatlacraiova/MainActivity.kt
+++ app/src/main/java/com/mamut/wechatlacraiova/MainActivity.kt
@@ -4,11 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -34,9 +32,12 @@ fun MainUI(modifier: Modifier = Modifier) {
     Scaffold (
         bottomBar = { ChatBar()},
         containerColor = Color(94, 94, 94, 255),
-        content = { innerPadding ->
-            LazyColumn(modifier = Modifier.padding(innerPadding)) {
-
+        content = { innerPadding -> Messages(innerPadding)},
+        topBar = {
+            Surface(modifier=Modifier.padding(75.dp)) {
+                Text(
+                    text="oh no"
+                )
             }
         }
     )
