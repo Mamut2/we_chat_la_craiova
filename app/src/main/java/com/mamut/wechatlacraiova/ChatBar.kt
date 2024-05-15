4e25cd81d32b63881f0218d34b4cff56395d5d9c
Mamut2 <65547296+Mamut2@users.noreply.github.com>
2024-05-15 19:07:15 +0300

    Updated chat bar

--- app/src/main/java/com/mamut/wechatlacraiova/ChatBar.kt
+++ app/src/main/java/com/mamut/wechatlacraiova/ChatBar.kt
@@ -1,32 +1,92 @@
 package com.mamut.wechatlacraiova
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowForward
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChatBar(modifier: Modifier = Modifier){
-    BottomAppBar(){
-        Row(
-            horizontalArrangement = Arrangement.End,
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxSize()
+    BottomAppBar(
+        contentPadding = PaddingValues(0.dp),
+        containerColor = Color.Transparent,
+        modifier = Modifier
+            .padding(10.dp, 0.dp, 10.dp, 20.dp)
+            .height(110.dp)
+    ){
+        Box(
+            modifier = Modifier
+                .background(
+                    color = Color(4, 155, 19, 255),
+                    shape = RoundedCornerShape(50.dp, 50.dp, 50.dp, 50.dp)
+                )
+                .border(
+                    width = 2.dp,
+                    Color.Black,
+                    shape = RoundedCornerShape(50.dp, 50.dp, 50.dp, 50.dp)
+                )
         ){
-            IconButton(
-                onClick = { },
-                content = {
-                    Icon(Icons.Filled.ArrowForward, contentDescription = null)
-                },
-            )
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxSize()
+            ){
+                var fieldText by remember { mutableStateOf("Enter text") }
+                TextField(
+                    value = fieldText, onValueChange = { fieldText = it},
+                    colors = TextFieldDefaults.colors(
+                        focusedContainerColor = Color.Transparent,
+                        unfocusedContainerColor = Color.Transparent,
+                        disabledContainerColor = Color.Transparent,
+                    ),
+                    modifier = Modifier
+                        .weight(85f)
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(50.dp, 50.dp, 50.dp, 50.dp))
+                )
+
+                IconButton(
+                    onClick = { },
+                    content = {
+                        Icon(Icons.Filled.ArrowForward, contentDescription = null)
+                    },
+                    modifier = Modifier
+                        .weight(15f)
+                        .fillMaxSize()
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/mamut/wechatlacraiova/MainActivity.kt
+++ app/src/main/java/com/mamut/wechatlacraiova/MainActivity.kt
@@ -7,12 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.mamut.wechatlacraiova.ui.theme.WeChatLaCraiovaTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +31,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainUI(modifier: Modifier = Modifier) {
-    Scaffold (bottomBar = { ChatBar()}){ innerPadding->
+    Scaffold (bottomBar = { ChatBar()}, containerColor = Color(94, 94, 94, 255)){ innerPadding->
         LazyColumn(modifier = Modifier.padding(innerPadding)) {
 
         }
