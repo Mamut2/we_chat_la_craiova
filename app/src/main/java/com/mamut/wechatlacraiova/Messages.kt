ec16b947ca27bd8f58cd557259072390e50b22fd
strumf007 <pozsonyistefan@gmail.com>
2024-05-16 18:30:35 +0300

    nicer ig

--- app/src/main/java/com/mamut/wechatlacraiova/Messages.kt
+++ app/src/main/java/com/mamut/wechatlacraiova/Messages.kt
@@ -1,31 +1,25 @@
 package com.mamut.wechatlacraiova
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.colorspace.ColorSpace
+import androidx.compose.ui.graphics.colorspace.ColorSpaces
 import androidx.compose.ui.unit.dp
+
 
 var texts = mutableStateListOf<String>()
 
@@ -55,7 +49,9 @@ fun Message(text: String){
         modifier = Modifier
             .fillMaxWidth()
             .padding(10.dp, 0.dp, 110.dp, 0.dp),
-        elevation = CardDefaults.cardElevation(4.dp)
+        elevation = CardDefaults.cardElevation(4.dp),
+        border = BorderStroke(2.dp, Color(red = 255f, green = 255f, blue = 255f, alpha = 255f)) ///AJUTOR DC NU SE SCHIMBA CULOAREA
+        //Color(186, 212, 170, 256))
     //.width(10.dp) - nu merge wtff
     ){
         Text(text = text)
