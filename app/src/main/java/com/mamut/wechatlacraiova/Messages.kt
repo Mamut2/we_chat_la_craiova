326234609ae15e6981bd9762edc12a3f63457d8a
strumf007 <pozsonyistefan@gmail.com>
2024-05-16 18:08:39 +0300

    nicer ig

--- app/src/main/java/com/mamut/wechatlacraiova/Messages.kt
+++ app/src/main/java/com/mamut/wechatlacraiova/Messages.kt
@@ -8,10 +8,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -51,6 +54,9 @@ fun Message(text: String){
     Card(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(10.dp, 0.dp, 110.dp, 0.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    //.width(10.dp) - nu merge wtff
     ){
         Text(text = text)
     }
