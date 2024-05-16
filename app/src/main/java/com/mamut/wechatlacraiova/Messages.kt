c3d529a50a11f81eca7b5cf9b44ed6acdbe2dc23
Mamut2 <65547296+Mamut2@users.noreply.github.com>
2024-05-16 20:42:00 +0300

    Chat system update

--- app/src/main/java/com/mamut/wechatlacraiova/Messages.kt
+++ app/src/main/java/com/mamut/wechatlacraiova/Messages.kt
@@ -49,10 +49,6 @@ fun Message(text: String){
         modifier = Modifier
             .fillMaxWidth()
             .padding(10.dp, 0.dp, 110.dp, 0.dp),
-        elevation = CardDefaults.cardElevation(4.dp),
-        border = BorderStroke(2.dp, Color(red = 255f, green = 255f, blue = 255f, alpha = 255f)) ///AJUTOR DC NU SE SCHIMBA CULOAREA
-        //Color(186, 212, 170, 256))
-        //.width(10.dp) - nu merge wtff
     ){
         Text(text = text)
     }
