256a6aab1a8372d30a2f0b3775b73aa2c32a5f3a
strumf007 <pozsonyistefan@gmail.com>
2024-05-18 00:11:31 +0300

    nuh uh

--- app/src/main/java/com/mamut/wechatlacraiova/MainActivity.kt
+++ app/src/main/java/com/mamut/wechatlacraiova/MainActivity.kt
@@ -1,62 +1,95 @@
 package com.mamut.wechatlacraiova
 
 import android.R
-import android.content.ContentValues
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.view.Window
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import com.google.firebase.FirebaseApp
-import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.mamut.wechatlacraiova.ui.theme.WeChatLaCraiovaTheme
+
+/*
+class OtherClass {
+    @Composable
+    fun accessListState() {
+        // Call the composable function to initialize listState
+        InitializeListState()
+
+        // Access the listState variable
+        MyViewModel.listState
+    }
+}
+ */
+
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        enableEdgeToEdge()
-        hideSystemUI(window)  // Hide navigation bar
-
         FirebaseApp.initializeApp(this)             // Firebase
         dbr = FirebaseDatabase.getInstance("https://wechatlacraiova-default-rtdb.europe-west1.firebasedatabase.app/").getReference()
         readMessages()
 
+        enableEdgeToEdge()
+        hideSystemUI(window)  // Hide navigation bar
+
         setContent {
             WeChatLaCraiovaTheme {
                 MainUI()
+
             }
         }
     }
 }
 
+@Preview
 @Composable
 fun MainUI(modifier: Modifier = Modifier) {
+    //InitializeListState()
+    val listState = rememberLazyListState()
     Scaffold (
         bottomBar = { ChatBar()},
         containerColor = Color(186, 212, 170, 255),
-        content = { innerPadding ->
-            ChatLog(innerPadding)
-        }
+        content = { innerPadding -> ChatLog(innerPadding) },
+        /*floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                          //absolut nmc, inca n-am reusit sa-l fac sa dea scroll in jos de aici
+                    //coroutineScope().launch { listState?.animateScrollToItem(index = listState!!.layoutInfo.totalItemsCount - 1) }
+                },
+                content = {Icon(Icons.Filled.KeyboardArrowDown, contentDescription = "Bottom of messaages")},
+                containerColor = Color(255, 233, 125, 255),
+                elevation = FloatingActionButtonDefaults.elevation(2.5.dp),
+                shape = CircleShape,
+                modifier = Modifier.padding(0.dp, 0.dp, 10.dp, 10.dp).size(30.dp)
+
+            )
+        }*/
     )
 }
 
@@ -80,3 +113,9 @@ private fun hideSystemUI(window: Window) {
         controller.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
     }
 }
+
+/*
+fun Scrolltolast(){
+    coroutineScope.launch{listState.animateScrollToItem(index = listState.layoutInfo.totalItemsCount - 1)}
+}
+*/
--- app/src/main/java/com/mamut/wechatlacraiova/Messages.kt
+++ app/src/main/java/com/mamut/wechatlacraiova/Messages.kt
@@ -1,35 +1,31 @@
 package com.mamut.wechatlacraiova
 
-import android.util.Log
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.Updater
-import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -39,10 +35,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.colorspace.ColorSpace
-import androidx.compose.ui.graphics.colorspace.ColorSpaces
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
@@ -51,8 +43,19 @@ import kotlinx.coroutines.launch
 
 var texts = mutableStateListOf<String>()
 
+/*
+object MyViewModel {
+    //companion object {
+        val listState: LazyListState? = null
+    //}
+}
+
+ */
+
+
 @Composable
 fun ChatLog(innerPadding:PaddingValues){
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -72,14 +75,7 @@ fun ChatLog(innerPadding:PaddingValues){
                     .fillMaxSize()
             )
             val listState = rememberLazyListState()
-
-            val reachedTop: Boolean by remember {
-                derivedStateOf {
-                    val firstVisibleItem = listState.layoutInfo.visibleItemsInfo.firstOrNull()
-                    firstVisibleItem?.index == 0
-                }
-            }
-
+            val coroutineScope = rememberCoroutineScope()
             LazyColumn(
                 state=listState,
                 verticalArrangement = Arrangement.spacedBy(5.dp),
@@ -93,18 +89,51 @@ fun ChatLog(innerPadding:PaddingValues){
                 }
             }
 
-            LaunchedEffect(key1 = finishedLoadingMessages) {
-                if(finishedLoadingMessages){
-                    listState.animateScrollToItem(index = listState.layoutInfo.totalItemsCount - 1)
-                    finishedLoadingMessages = false
-                }
-            }
+            Surface(
+                modifier = Modifier
+                    .align(Alignment.End)
+                    .padding(0.dp, 0.dp, 40.dp, 20.dp),
+                shape = RoundedCornerShape(25.dp)
+            )
+            {
 
-            LaunchedEffect(reachedTop) {
-                if(reachedTop) loadOldMessages()
+                /*
+                IconButton(content = {
+                    Icon(
+                        Icons.Filled.KeyboardArrowDown,
+                        contentDescription = null
+                    )
+                },
+                    onClick = { coroutineScope.launch { listState.animateScrollToItem(index = listState.layoutInfo.totalItemsCount - 1) } }
+                )
+                */
+                var opening by remember { mutableStateOf(true) }
+                LaunchedEffect(key1 = opening) {
+                    if(opening){
+                        delay(500)
+                        coroutineScope.launch{listState.animateScrollToItem(index = listState.layoutInfo.totalItemsCount - 1)}
+                        opening=false
+                    }
+                }
+
             }
         }
     }
+    FloatingActionButton(
+
+        content = {Icon(Icons.Filled.KeyboardArrowDown, contentDescription = "Bottom of messaages")},
+        containerColor = Color(255, 233, 125, 255),
+        elevation = FloatingActionButtonDefaults.elevation(2.5.dp),
+        shape = CircleShape,
+
+        modifier = Modifier
+            .size(100.dp),
+        onClick = {
+            //coroutineScope.launch { listState.animateScrollToItem(index = listState.layoutInfo.totalItemsCount - 1)}
+            //absolut nmc, inca n-am reusit sa-l fac sa dea scroll in jos de aici
+            //coroutineScope().launch { listState?.animateScrollToItem(index = listState!!.layoutInfo.totalItemsCount - 1) }
+        }
+    )
 }
 
 @Composable
@@ -124,3 +153,4 @@ fun Message(text: String){
         )
     }
 }
+
